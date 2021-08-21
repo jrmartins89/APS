b325741b6236a1123f881655f7abfb509641908c
Jose Ribamar Junior <jribamarjunior89@gmail.com>
2021-08-21 01:10:44 -0300

    edição dos componentes da tela de login do segundo jogador

--- mvc/view/tela_login_jogador.py
+++ mvc/view/tela_login_jogador.py
@@ -8,6 +8,7 @@ class TelaLoginJogador(Tela):
     def __init__(self, controlador_jogador):
         self.__controlador_jogador = controlador_jogador
         self.__window_login = None
+        self.__window_login_segundo_jogador = None
 
     def init_components(self):
         layout_login_jogador = [
@@ -26,6 +27,24 @@ class TelaLoginJogador(Tela):
                                         element_justification='c'
                                         ).Layout(layout_login_jogador)
 
+    def init_components_segundo_jogador(self):
+        layout_login_segundo_jogador = [
+                                    [sG.Text('Segundo Jogador', font=('Times', 30))],
+                                    [sG.Text('Faça o login!', font=('Times', 30))],
+                                    [sG.Text('*Apelido', font=('Times', 14))],
+                                    [sG.InputText('', key='apelido')],
+                                    [sG.Text('*Senha', font=('Times', 14))],
+                                    [sG.InputText('', key='senha')],
+                                    [sG.OK()],
+                                    [sG.Button('Voltar')]
+                               ]
+
+        self.__window_login_segundo_jogador = sG.Window('Login no Jogo',
+                                        font=('Times', 15),
+                                        size=(600, 350),
+                                        element_justification='c'
+                                        ).Layout(layout_login_segundo_jogador)
+
     def close(self):
         self.__window_login.Close()
 
@@ -36,8 +55,8 @@ class TelaLoginJogador(Tela):
         return button, values
 
     def open_tela_login_segundo_jogador(self):
-        self.init_components()
-        button, values = self.__window_login.Read()
+        self.init_components_segundo_jogador()
+        button, values = self.__window_login_segundo_jogador.Read()
         self.close()
         return button, values
 
