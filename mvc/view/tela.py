851f294d73e5dbdfa0294a38df5e58ba127575d3
Jose Ribamar Junior <jribamarjunior89@gmail.com>
2021-08-23 20:53:28 -0300

    ajuste da identação

--- mvc/view/tela.py
+++ mvc/view/tela.py
@@ -2,10 +2,10 @@ from abc import ABC, abstractmethod
 
 
 class Tela(ABC):
-  @abstractmethod
-  def __init__(self, controlador):
-    self.__controlador = controlador
+    @abstractmethod
+    def __init__(self, controlador):
+        self.__controlador = controlador
 
-  def mostra_lista_str(self, lista: []):
-    for elem in lista:
-      print(elem)
+    def mostra_lista_str(self, lista: []):
+        for elem in lista:
+            print(elem)
